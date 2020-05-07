10557025e718dbce14a8ab7f1a2d2a0ecc3fc57b
Luís Gomes <luismsgomes@gmail.com>
2020-05-07 15:06:30 +0100

    simplified core

--- examples/mpseq-core-example.py
+++ examples/mpseq-core-example.py
@@ -1,46 +1,72 @@
+from os import getpid
 from pseq.core import ParallelSequenceProcessor, Producer, Processor, Consumer
 from random import random, choice
 from time import sleep
-import logging
 
 
 class TimeProducer(Producer):
+    def __init__(self):
+        print(f"TimeProducer.__init__() @pid={getpid()}")
+
+    def init(self):
+        print(f"TimeProducer.init() @pid={getpid()}")
+
     def produce(self):
-        for _ in range(20):
+        print(f"TimeProducer.produce() @pid={getpid()}")
+        for _ in range(10):
             yield random() * 3
 
     def shutdown(self):
-        print("Producer shutting down")
+        print(f"TimeProducer.shutdown() @pid={getpid()}")
+
 
 class TimeProcessor(Processor):
+    def __init__(self):
+        print(f"TimeProcessor.__init__() @pid={getpid()}")
+
+    def init(self):
+        print(f"TimeProcessor.init() @pid={getpid()}")
+
     def process(self, secs):
-        print(f"Sleeping {secs} secs")
+        print(f"TimeProcessor.process(secs={secs:.2f}) @pid={getpid()}")
         slept_secs = choice([secs, secs, secs / 2])
         sleep(slept_secs)
         if choice([True, False, False, False]):
-            raise Exception
+            raise Exception("example exception")
         return slept_secs
 
     def shutdown(self):
-        print("Processor shutting down")
+        print(f"TimeProcessor.shutdown() @pid={getpid()}")
+
 
 class TimeConsumer(Consumer):
+    def __init__(self):
+        print(f"TimeConsumer.__init__() @pid={getpid()}")
+
+    def init(self):
+        print(f"TimeConsumer.init() @pid={getpid()}")
+
     def consume(self, given_secs, slept_secs, exception):
-        print(f"Tried to sleep {given_secs}, slept {slept_secs}; exception {repr(exception)}")
+        print(
+            f"TimeConsumer.consume(given_secs={given_secs:.2f},"
+            + " slept_secs=" + ("None" if slept_secs is None else f"{slept_secs:.2f}")
+            + f" exception={exception!r}) @pid={getpid()}"
+        )
 
     def shutdown(self):
-        print("Consumer shutting down")
+        print(f"TimeConsumer.shutdown() @pid={getpid()}")
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    processor = ParallelSequenceProcessor(TimeProducer(), TimeProcessor(), TimeConsumer(), 2)
+    print(f"main() @pid={getpid()}")
+    processor = ParallelSequenceProcessor(
+        TimeProducer(), TimeProcessor(), TimeConsumer(), n_processors=2
+    )
     processor.start()
 
-    print("Hello from main process.")
     # could do other things here
 
-    processor.join() # wait for all sub-procs to finish
+    processor.join()  # wait for all sub-procs to finish
 
 
 if __name__ == "__main__":
--- src/pseq/core.py
+++ src/pseq/core.py
@@ -2,10 +2,7 @@ import datetime
 import itertools
 import os
 import multiprocessing
-import logging
-
-
-LOG = logging.getLogger(__name__)
+import traceback
 
 
 class PipelineComponent(object):
@@ -38,33 +35,6 @@ class WorkUnit(object):
         self.result = None
         self.exception = None
 
-    def log_event(self, event):
-        LOG.debug(f"{self} {event}")
-
-    def __str__(self):
-        return f"{self.__class__.__name__}-{self.serial}"
-
-    class Event(object):
-        def __init__(self, *args, **kwargs):
-            self.ts = datetime.datetime.now()
-            self.args = args
-            self.kwargs = kwargs
-
-        def __str__(self):
-            s = [f"{self.__class__.__name__} {self.ts}"]
-            s.extend(map(str, self.args))
-            for k, v in sorted(self.kwargs.items()):
-                s.append(f"{k}={repr(v)}")
-            return " ".join(s)
-
-    class Produced(Event): pass
-    class Queued(Event): pass
-    class Executing(Event): pass
-    class Executed(Event): pass
-    class OnHold(Event): pass
-    class Consuming(Event): pass
-    class Consumed(Event): pass
-
 
 class Shutdown(WorkUnit):
     def __init__(self, serial):
@@ -77,36 +47,36 @@ def process(processor, todo_queue, done_queue):
     work_unit = None
     while not isinstance(work_unit, Shutdown):
         work_unit = todo_queue.get()
-        work_unit.log_event(WorkUnit.Executing(pid=pid))
         if isinstance(work_unit, Shutdown):
             try:
                 processor.shutdown()
             except:
+                traceback.print_exc()
                 pass
         else:
             try:
                 work_unit.result = processor.process(work_unit.data)
             except Exception as exception:
-                work_unit.exception = exception
-        work_unit.log_event(WorkUnit.Executed(pid=pid))
+               work_unit.exception = exception
         done_queue.put(work_unit)
 
 
 def produce(producer, todo_queue, n_processors):
     producer.init()
     serial = itertools.count(start=1)
-    for data in producer.produce():
-        work_unit = WorkUnit(next(serial), data)
-        work_unit.log_event(WorkUnit.Produced())
-        todo_queue.put(work_unit)
-        work_unit.log_event(WorkUnit.Queued())
-    producer.shutdown()
+    try:
+        for data in producer.produce():
+            work_unit = WorkUnit(next(serial), data)
+            todo_queue.put(work_unit)
+    except:  # if producer.produce() crashes
+        traceback.print_exc()
+    try:
+        producer.shutdown()
+    except:
+        traceback.print_exc()
     for _ in range(n_processors):
         shutdown_unit = Shutdown(next(serial))
-        shutdown_unit.log_event(WorkUnit.Produced())
         todo_queue.put(shutdown_unit)
-        shutdown_unit.log_event(WorkUnit.Queued())
-
 
 def get_done_work_units(done_queue, n_processors):
     n_running = n_processors
@@ -132,7 +102,6 @@ def arrange_work_units_in_order(work_units):
                 yield held_unit
                 next_serial = next(serial)
         else:
-            work_unit.log_event(WorkUnit.OnHold())
             on_hold[work_unit.serial] = work_unit
     assert not on_hold
 
@@ -143,10 +112,11 @@ def consume(consumer, done_queue, n_processors, require_in_order):
     if require_in_order:
         work_units = arrange_work_units_in_order(work_units)
     for work_unit in work_units:
-        work_unit.log_event(WorkUnit.Consuming())
         if not isinstance(work_unit, Shutdown):
-            consumer.consume(work_unit.data, work_unit.result, work_unit.exception)
-        work_unit.log_event(WorkUnit.Consumed())
+            try:
+                consumer.consume(work_unit.data, work_unit.result, work_unit.exception)
+            except:
+                traceback.print_exc()
     consumer.shutdown()
 
 
@@ -168,7 +138,7 @@ class ParallelSequenceProcessor(object):
 
         """
         if mp_context is None:
-            mp_context = multiprocessing.get_context(method='forkserver')
+            mp_context = multiprocessing.get_context(method="forkserver")
         if require_in_order is None:
             require_in_order = True
         if n_processors is None:
@@ -182,7 +152,6 @@ class ParallelSequenceProcessor(object):
         self.n_processors = n_processors
         self.todo_queue = mp_context.Queue(maxsize=2 * n_processors)
         self.done_queue = mp_context.Queue(maxsize=2 * n_processors)
-        LOG.info(f"{self}: creating producer and consumer processes")
         self.procs = [
             mp_context.Process(
                 target=produce, args=(producer, self.todo_queue, n_processors),
@@ -192,7 +161,6 @@ class ParallelSequenceProcessor(object):
                 args=(consumer, self.done_queue, n_processors, require_in_order),
             ),
         ]
-        LOG.info(f"{self}: creating {n_processors} processor processes")
         self.procs.extend(
             mp_context.Process(
                 target=process, args=(processor, self.todo_queue, self.done_queue)
@@ -201,16 +169,12 @@ class ParallelSequenceProcessor(object):
         )
 
     def start(self):
-        LOG.info(f"{self}: starting {len(self.procs)} sub-processes")
         for proc in self.procs:
             proc.start()
-        LOG.info(f"{self}: started {len(self.procs)} sub-processes")
 
     def join(self):
-        LOG.info(f"{self}: waiting for {len(self.procs)} sub-processes")
         for proc in self.procs:
             proc.join()
-        LOG.info(f"{self}: all {len(self.procs)} sub-processes terminated")
 
     def __str__(self):
         return f"{self.__class__.__name__}()"
